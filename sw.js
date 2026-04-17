80f03b68095495700d41e2e0ffbade7842cfac1d
salnasiri34-stack <salnasiri34@gmail.com>
2026-04-17 13:20:59 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,1 @@
-self.addEventListener('push', function(event) {
-    const options = {
-        body: 'تم إرسال تحذير! يرجى التصرف بهدوء ومتابعة التعليمات.',
-        icon: 'https://cdn-icons-png.flaticon.com/512/179/179386.png',
-        vibrate: [500, 100, 500, 100, 500],
-        data: { dateOfArrival: Date.now() }
-    };
-    event.waitUntil(self.registration.showNotification('⚠️ تحذيرات سلومي', options));
-});
+importScripts('https://cdn.webpushr.com/sw-v2.js');
