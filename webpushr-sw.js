ceb8f9cc985441715971c2a996c94285895515bf
salnasiri34-stack <salnasiri34@gmail.com>
2026-04-17 21:44:59 +0300

    Update webpushr-sw.js

--- webpushr-sw.js
+++ webpushr-sw.js
@@ -1,10 +1,18 @@
 importScripts('https://cdn.webpushr.com/sw-server.js');
 
+// استجابة ذكية واهتزاز عسكري
 self.addEventListener('push', function(event) {
     const options = {
-        body: 'تنبيه طوارئ من العراب سلومي! 🚨',
+        body: 'إنذار طوارئ عاجل من نظام العرّاب سلومي! 🚨',
         icon: 'https://cdn-icons-png.flaticon.com/512/559/559343.png',
-        vibrate: [500, 100, 500, 100, 500]
+        badge: 'https://cdn-icons-png.flaticon.com/512/559/559343.png',
+        vibrate: [500, 110, 500, 110, 500],
+        data: { url: '/alert/' }
     };
-    event.waitUntil(self.registration.showNotification('⚠️ إنذار عاجل', options));
+    event.waitUntil(self.registration.showNotification('⚠️ تنبيه أمني عاجل', options));
+});
+
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    event.waitUntil(clients.openWindow('/alert/'));
 });
