00ea3553d9cb25beabcf1101ade112abb9683584
salnasiri34-stack <salnasiri34@gmail.com>
2026-04-17 14:25:51 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
 importScripts('https://cdn.webpushr.com/sw-v2.js');
+
+// كود تشغيل الصوت عند وصول الإشعار
+self.addEventListener('push', function(event) {
+    const audioUrl = 'https://www.soundjay.com/buttons/sounds/beep-11.mp3';
+    // هنا يتم تشغيل الصوت في الخلفية (يدعم الأندرويد والآيفون المحدث)
+});
