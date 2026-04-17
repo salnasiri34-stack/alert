59b32d8b3dc56c624ab18e00c8d0e92fd331ff05
salnasiri34-stack <salnasiri34@gmail.com>
2026-04-17 16:09:47 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,1 @@
-self.addEventListener('push', function(event) {
-    const data = event.data.json();
-    event.waitUntil(
-        self.registration.showNotification(data.title, {
-            body: data.message,
-            icon: 'https://img.icons8.com/emoji/96/alert-light.png'
-        })
-    );
-});
+importScripts('https://cdn.webpushr.com/sw-server.js');
