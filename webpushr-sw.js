e73198112289bbfd4c1881104436d5d77c7f7252
salnasiri34-stack <salnasiri34@gmail.com>
2026-04-17 18:56:55 +0300

    Update webpushr-sw.js

--- webpushr-sw.js
+++ webpushr-sw.js
@@ -1,1 +1,9 @@
 importScripts('https://cdn.webpushr.com/sw-server.js');
+
+// استجابة ذكية عند الضغط على الإشعار
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    event.waitUntil(
+        clients.openWindow('/alert/')
+    );
+});
